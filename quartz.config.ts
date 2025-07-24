9a7885cfbe470200fd0e03a156096721a6915bf2
rival <samposalmela111@gmail.com>
2025-07-24 19:06:19 +0300

    Updated title to Druptur

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Farfinder",
+    pageTitle: "Druptur",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
