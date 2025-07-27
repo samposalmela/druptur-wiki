c05e9357c22de1863a386c9f3757171bc122c41e
rival <samposalmela111@gmail.com>
2025-07-27 19:52:23 +0300

    Quartz sync: Jul 27, 2025, 7:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -45,7 +45,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
+          secondary: "#880808",
           tertiary: "#880808",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
